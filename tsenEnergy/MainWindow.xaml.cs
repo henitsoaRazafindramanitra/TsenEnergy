ae6153bfa19bea3e68d3d2b03bdd6a55a08b5d97
henitsoaRazafindramanitra <h.razafindramanitra@gmail.com>
2023-07-11 13:23:59 +0300

    version1

---
+++ tsenEnergy/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tsenEnergy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_inscription_Click(object sender, RoutedEventArgs e)
+        {
+            //InscriptionForm inscriptionForm = new InscriptionForm();
+            //inscriptionForm.Show();
+
+            this.Hide();
+
+        }
+
+        private void bt_identifier_Click(object sender, RoutedEventArgs e)
+        {
+            // LoginForm loginForm = new LoginForm();
+            //loginForm.Show();
+
+           
+           // this.Hide();
+        }
+    }
+}
