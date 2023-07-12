09928c579bc35eef8e90b6ad231085cefc5c5e09
RltCodes <lalahtahiana@gmail.com>
2023-07-12 11:58:08 +0300

    interface validation inscription fournisseur

---
+++ tsenEnergy/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tsenenergy
+{
+    internal class Admin
+    {
+        string nom { get; set; }
+        string email { get; set; }
+        string motDePasse { get; set; }
+
+        public Admin(string nom, string email, string motDePasse)
+        {
+            nom = nom;
+            email = email;
+            motDePasse = motDePasse;
+        }
+    }
+}
