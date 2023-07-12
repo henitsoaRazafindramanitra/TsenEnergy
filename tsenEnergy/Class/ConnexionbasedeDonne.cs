8b7ecd4ca36c950ace4d76888c006f78e5ea61f7
ony <rabenjarijaonaoa@gmail.com>
2023-07-12 11:13:57 +0300

    interface et fonctionalite Authentification

---
+++ tsenEnergy/Class/ConnexionbasedeDonne.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace tsenEnergy.Class
+{
+    internal class ConnexionbasedeDonne
+    {
+        private NpgsqlConnection connection;
+        private readonly string connectionString = "Server=localhost;Port=5432;Database=GestionsDesAbonnes;User Id=postgres;Password=ronyandrihanina;";
+
+        public ConnexionbasedeDonne()
+        {
+            connection = new NpgsqlConnection(connectionString);
+        }
+
+        public NpgsqlConnection Connection
+        {
+            get { return connection; }
+            set { connection = value; }
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connexion à la base de données réussie!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur lors de la connexion à la base de données : " + ex.Message);
+            }
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                connection.Close();
+                Console.WriteLine("Déconnexion à la base de données réussie !");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur lors de la déconnexion à la base de données : " + ex.Message);
+            }
+        }
+    }
+}
